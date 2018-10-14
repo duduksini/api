73e5c7f99aaa8761e021fe1904a9eb3f05270754
ohgo <aries@ariesku.com>
2018-10-14 11:27:44 +0200

    enable log templating

--- src/middlewares/request-response-logger.ts
+++ src/middlewares/request-response-logger.ts
@@ -2,6 +2,6 @@ import * as responseTime from 'response-time';
 import log from '../utilities/log';
 
 const logResponseRequest = (req, res, time) =>
-  log.info('Returned %s in %d ms for %s %s', res.statusCode, time, req.method, req.url);
+  log.info('%s %s returned %s in %d ms', req.method, req.url, res.statusCode, time);
 
 export default responseTime(logResponseRequest);
--- src/utilities/log.ts
+++ src/utilities/log.ts
@@ -2,7 +2,10 @@ import * as winston from 'winston';
 
 const log : winston.Logger = winston.createLogger({
   level: process.env.LogLevel || 'info',
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.splat(),
+    winston.format.json(),
+  ),
   transports: [
     new winston.transports.Console(),
   ],
